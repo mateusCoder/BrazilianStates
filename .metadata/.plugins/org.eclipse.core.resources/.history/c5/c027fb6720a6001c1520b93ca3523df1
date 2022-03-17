66a05f79a7d2b57f580b8959de17e03908f58c08
mateusCoder <mateus.moraes0507@gmail.com>
2022-03-17 15:48:22 -0300

    Add GET Filter By Region

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c5/c027fb6720a6001c1520b93ca3523df1
@@ -0,0 +1,9 @@
+package br.com.compass.brazilianStates.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.compass.brazilianStates.model.State;
+
+public interface StateRepository  extends JpaRepository<State, Long>{
+	
+}
--- BrazilianStates/src/main/java/br/com/compass/brazilianStates/controller/StateController.java
+++ BrazilianStates/src/main/java/br/com/compass/brazilianStates/controller/StateController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +14,21 @@ import br.com.compass.brazilianStates.model.TypeRegion;
 import br.com.compass.brazilianStates.repository.StateRepository;
 
 @RestController
+@RequestMapping("/states")
 public class StateController {
 	
 	@Autowired
 	private StateRepository stateRepository;
 	
-	@RequestMapping("/states")
-	public List<StateDTO> list(){	
-		List<State> states = stateRepository.findAll();
-		return StateDTO.convertToDto(states);
+	@GetMapping
+	public List<StateDTO> list(TypeRegion region){	
+		
+		if(region == null) {
+			List<State> states = stateRepository.findAll();
+			return StateDTO.convertToDto(states);
+		}else {
+			List<State> states = stateRepository.findByRegion(region);
+			return StateDTO.convertToDto(states);
+		}
 	}
 }
