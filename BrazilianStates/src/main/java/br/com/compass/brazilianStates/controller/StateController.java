3cc68c6d801d696f827414be9d25e8dd9b8f9ee8
mateusCoder <mateus.moraes0507@gmail.com>
2022-03-17 17:49:44 -0300

    Add GET for state details

--- BrazilianStates/src/main/java/br/com/compass/brazilianStates/controller/StateController.java
+++ BrazilianStates/src/main/java/br/com/compass/brazilianStates/controller/StateController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,5 +47,11 @@ public class StateController {
 		
 		URI uri = uriBuilder.path("/states/{id}").buildAndExpand(state.getId()).toUri();
 		return ResponseEntity.created(uri).body(new StateDTO(state));
+	}
+	
+	@GetMapping("/{id}")
+	public StateDTO check(@PathVariable Long id) {
+		State state = stateRepository.getById(id);
+		return new StateDTO(state);
 	}
 }
