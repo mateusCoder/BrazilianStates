8f48538b4080ed571ee40274fcdd6b4c492be484
mateusCoder <mateus.moraes0507@gmail.com>
2022-03-19 15:07:47 -0300

    Add Swagger to Brazilian States API

---
+++ BrazilianStates/src/main/java/br/com/compass/brazilianStates/config/cors/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package br.com.compass.brazilianStates.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("https://editor.swagger.io")
+			.allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
+}
